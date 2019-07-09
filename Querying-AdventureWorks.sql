b93fd9f908a33c65830f282866b5f84d6da8ea41
Naji El Kotob <naji@dotnetheroes.com>
2019-07-09 07:16:54 +0300

    Update Querying-AdventureWorks.sql

--- Querying-AdventureWorks.sql
+++ Querying-AdventureWorks.sql
@@ -10,3 +10,12 @@ ______          ______               _
 */
 
 -- T-SQL Querying (AdventureWorks)
+
+-- Reports information about a specified database or all databases.
+-- https://docs.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sp-helpdb-transact-sql
+sp_helpdb 
+
+-- Using sp_datatype_info to get the data type of a variable
+-- https://docs.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sp-datatype-info-transact-sql
+sp_datatype_info
+
